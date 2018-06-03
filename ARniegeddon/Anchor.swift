f2b8e33c11668f912f87eb9ed471759fb024a5ec
Booharin <booharin@bk.ru>
2018-06-03 22:23:11 +0300

    Add Level

---
+++ ARniegeddon/Anchor.swift
@@ -0,0 +1,13 @@
+//
+//  Anchor.swift
+//  ARniegeddon
+//
+//  Created by Александр on 03.06.2018.
+//  Copyright © 2018 Ray Wenderlich. All rights reserved.
+//
+
+import ARKit
+
+class Anchor: ARAnchor {
+    var type: NodeType?
+}
